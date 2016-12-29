7d20ee438c35d914df311e05630b72d2c126845e
WangYihang <wangyihanger@gmail.com>
2016-12-29 22:48:29 +0800

    half

--- HTML2Python.py
+++ HTML2Python.py
@@ -3,6 +3,7 @@
 
 import requests
 import sys
+import bs4
 
 
 def getScriptFileName():
@@ -44,9 +45,160 @@ def downloadFile(url):
     return fileName
 
 
+def handle_ul(soup):
+    al_all = soup.findAll("ul")
+    for al in al_all:
+        li_all = al.findAll("li")
+        for li in li_all:
+            print "* " + str(li.contents)
+
+
+def handle_ol(soup):
+    al_all = soup.findAll("ol")
+    for al in al_all:
+        li_all = al.findAll("li")
+        for li in li_all:
+            print "* " + str(li.contents)
+
+
+def handle_h1(soup):
+    h1s = soup.findAll("h1")
+    for h1 in h1s:
+        print "# " + str(h1.contents)
+    return ""
+
+
+def handle_h2(soup):
+    h2s = soup.findAll("h2")
+    for h2 in h2s:
+        print "## " + str(h2.contents)
+    return ""
+
+
+def handle_h3(soup):
+    h3s = soup.findAll("h3")
+    for h3 in h3s:
+        print "### " + str(h3.contents)
+    return ""
+
+
+def handle_h4(soup):
+    h4s = soup.findAll("h4")
+    for h4 in h4s:
+        print "#### " + str(h4.contents)
+    return ""
+
+
+def handle_h5(soup):
+    h5s = soup.findAll("h5")
+    for h5 in h5s:
+        print "##### " + str(h5.contents)
+    return ""
+
+
+def handle_em(soup):
+    ems = soup.findAll("em")
+    for em in ems:
+        print "*" + str(em.contents) + "*"
+    return ""
+
+
+def handle_strong(soup):
+    strongs = soup.findAll("strong")
+    for strong in strongs:
+        print "**" + str(strong.contents) + "**"
+    return ""
+
+
+def handle_img(soup):
+    imgs = soup.findAll("img")
+    for img in imgs:
+        describe = ""
+        if img.has_attr('alt'):
+            describe = img['alt']
+        else:
+            describe = "Picture"
+        print "![" + describe + "](" + img['src'] + ")"
+    return ""
+
+
+def handle_code(soup):
+    codes = soup.findAll("code")
+    for code in codes:
+        # deault one tag only has one attr
+        if code.has_attr('class'):
+            if code['class'][0] == "prettyprint":
+                print "`" + str(code.contents) + "`"
+            else:
+                print "```\r\n" + str(code.contents) + "\r\n```"
+        else:
+            print "```\r\n" + str(code.contents) + "\r\n```"
+    return ""
+
+
+def handle_blockquote(soup):
+    blockquotes = soup.findAll("blockquote")
+    for blockquote in blockquotes:
+        for line in blockquote:
+            lineContent = str(line)
+            if len(lineContent) == 0:
+                continue
+            elif len(lineContent) < 2:
+                if lineContent == "\n":
+                    continue
+                if lineContent == "\r":
+                    continue
+                if lineContent == "\r\n":
+                    continue
+            else:
+                for line in lineContent.split("\n"):
+                    print "> " + line
+    return ""
+
+
+def handle_hr(soup):
+    blockquotes = soup.findAll("blockquote")
+    for blockquote in blockquotes:
+        print "\r\n-\r\n"
+    return ""
+
+
 def convert(content):
     markdown = ""
+    soup = bs4.BeautifulSoup(content, "html.parser")
+    print "-----------------"
+    handle_ul(soup)
+    print "-----------------"
+    handle_ol(soup)
+    print "-----------------"
+    handle_h1(soup)
+    print "-----------------"
+    handle_h2(soup)
+    print "-----------------"
+    handle_h3(soup)
+    print "-----------------"
+    handle_h4(soup)
+    print "-----------------"
+    handle_h5(soup)
+    print "-----------------"
+    handle_em(soup)
+    print "-----------------"
+    handle_strong(soup)
+    print "-----------------"
+    handle_img(soup)
+    print "-----------------"
+    handle_code(soup)
+    print "-----------------"
+    handle_blockquote(soup)
+    print "-----------------"
+    handle_hr(soup)
     return markdown
+
+
+def appendLineToFile(fileName, line):
+    file = open(fileName, "a+")
+    file.write(line + "\r\n")
+    file.close()
 
 
 def writeFile(fileName, content):
